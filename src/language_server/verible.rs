bf9612662612bba26e5240836b6f71ee8fac45d1
Owen Law <owenlaw222@gmail.com>
2026-07-14 16:17:15 -0400

    Bump verible to v0.0-4080

--- src/language_server/verible.rs
+++ src/language_server/verible.rs
@@ -9,7 +9,7 @@ pub struct Verible {
 impl LanguageServer for Verible {
     const LANGUAGE_SERVER_ID: &'static str = "verible";
     const DOWNLOAD_REPO: &'static str = "chipsalliance/verible";
-    const DOWNLOAD_TAG: &'static str = "v0.0-4053-g89d4d98a";
+    const DOWNLOAD_TAG: &'static str = "v0.0-4080-ga0a8d8eb";
 
     fn get_cached_binary(&self) -> Option<String> {
         self.cached_binary.clone()
